a77c77bc25ee110785854e5d6f3b9df07c24487b
Ron Rechenmacher <ron@fnal.gov>
2026-07-06 16:49:41 -0500

    non-plugin ICEBERGChannelMapV100.hpp

---
+++ plugins/ICEBERGChannelMapV100.hpp
@@ -0,0 +1,79 @@
+#ifndef ICEBERGCHANNELMAPV100_HPP_
+#define ICEBERGCHANNELMAPV100_HPP_
+
+#include "detchannelmaps/TPCChannelMap.hpp"
+#include "PD2HDChannelMapSP.h"
+
+#include <TRACE/trace.h>
+
+namespace dunedaq {
+namespace detchannelmaps {
+
+class ICEBERGChannelMapV100 :  public TPCChannelMap
+{
+public:
+
+  explicit ICEBERGChannelMapV100() {
+
+    const char* detchannelmaps_share_cstr = getenv("DETCHANNELMAPS_SHARE");
+    if (!detchannelmaps_share_cstr) {
+      throw std::runtime_error("Environment variable DETCHANNELMAPS_SHARE is not set");
+    }
+    std::string detchannelmaps_share(detchannelmaps_share_cstr);
+    std::string channel_map_file = detchannelmaps_share + "/config/iceberg/iceberg_wibeth_chanmap_v100.txt";
+    m_channel_map.reset(new dune::PD2HDChannelMapSP());
+    m_channel_map->ReadMapFromFile(channel_map_file);
+    TLOG(10) << "ICEBERGChannelMapV100 Created";
+  }
+
+  ICEBERGChannelMapV100(const ICEBERGChannelMapV100&) = delete;
+  ICEBERGChannelMapV100& operator=(const ICEBERGChannelMapV100&) = delete;
+  ICEBERGChannelMapV100(ICEBERGChannelMapV100&&) = delete;
+  ICEBERGChannelMapV100& operator=(ICEBERGChannelMapV100&&) = delete;
+
+  uint
+  get_offline_channel_from_crate_slot_fiber_chan(uint crate, uint slot, uint link, uint wibframechan) final {
+    auto chan_info = m_channel_map->GetChanInfoFromWIBElements(
+        crate, slot, link, wibframechan
+    );
+    if (!chan_info.valid) {
+      return -1;
+    }
+    return chan_info.offlchan;
+  }
+
+  uint
+  get_plane_from_offline_channel(uint offchannel) final {
+    auto chan_info = m_channel_map->GetChanInfoFromOfflChan(offchannel);
+    if (!chan_info.valid) {
+      return 9999;
+    }
+    return chan_info.plane;
+  };
+
+  std::string
+  get_tpc_element_from_offline_channel(uint offchannel) final {
+    auto chan_info = m_channel_map->GetChanInfoFromOfflChan(offchannel);
+    if (!chan_info.valid) {
+      return "";
+    }
+    return chan_info.APAName;
+  };
+
+  std::optional<TPCCoords>
+  get_crate_slot_fiber_chan_from_offline_channel(uint offchannel) {
+    auto ci = m_channel_map->GetChanInfoFromOfflChan(offchannel);
+    if ( !ci.valid) {
+      return std::nullopt;
+    }
+    return TPCCoords{ci.crate, ci.wib-1, ci.link, ci.wibframechan};
+  }
+
+private:
+  std::unique_ptr<dune::PD2HDChannelMapSP> m_channel_map;
+};
+
+} // namespace detchannelmaps
+} // namespace dunedaq
+
+#endif // ICEBERGCHANNELMAPV100_HPP_
